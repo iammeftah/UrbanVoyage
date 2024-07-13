d55a246ae887035986877c52f3cc137f23ea9220
itserror808 <meftahahmedreda02@gmail.com>
2024-07-13 16:51:34 +0100

    Auth done + Frontend zwiw9at

--- UrbanVoyageFrontend/src/app/pages/booking-page/booking-page.component.ts
+++ UrbanVoyageFrontend/src/app/pages/booking-page/booking-page.component.ts
@@ -6,10 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./booking-page.component.css']
 })
 export class BookingPageComponent {
-  isDropdownOpen: boolean = false;
   seatTypes: string[] = ['Standard Seat', 'Premium Seat', 'VIP Seat'];
+  selectedSeatTypeIndex: number | null = null;
 
-  toggleDropdown(): void {
-    this.isDropdownOpen = !this.isDropdownOpen;
+  selectSeatType(index: number): void {
+    this.selectedSeatTypeIndex = index;
+  }
+
+  getSeatTypeButtonClass(index: number): string {
+    const baseClasses = 'w-full flex-1 py-2 px-4 rounded-full text-sm font-medium transition-all duration-300 focus:outline-none ';
+    const activeClasses = 'w-full bg-cyan-600 text-white hover:bg-cyan-700 ';
+    const inactiveClasses = 'w-full bg-gray-200 text-gray-700 hover:bg-gray-300 focus:ring-gray-400 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600';
+
+    return `${baseClasses} ${this.selectedSeatTypeIndex === index ? activeClasses : inactiveClasses}`;
   }
 }
