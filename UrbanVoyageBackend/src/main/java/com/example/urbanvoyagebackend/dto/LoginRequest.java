6f478797046c5635b6f7e282c4d4a1d85a510c45
itserror808 <meftahahmedreda02@gmail.com>
2024-07-14 12:45:49 +0100

    sunday 14 - 12:45

--- UrbanVoyageBackend/src/main/java/com/example/urbanvoyagebackend/dto/LoginRequest.java
+++ UrbanVoyageBackend/src/main/java/com/example/urbanvoyagebackend/dto/LoginRequest.java
@@ -1,10 +1,6 @@
 package com.example.urbanvoyagebackend.dto;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Setter
-@Getter
 public class LoginRequest {
     // Getters and setters
     private String email;
@@ -16,6 +12,22 @@ public class LoginRequest {
 
     public LoginRequest(String email, String password) {
         this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 }
