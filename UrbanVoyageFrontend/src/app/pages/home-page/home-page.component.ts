2277f4c961339142136083ec57467a0fb3d4eb85
itserror808 <meftahahmedreda02@gmail.com>
2024-07-21 02:01:17 +0100

    Ticket Order almost Done -> Update ticket price by updating seatType

--- UrbanVoyageFrontend/src/app/pages/home-page/home-page.component.ts
+++ UrbanVoyageFrontend/src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,43 @@
-import {Component, ElementRef, HostListener, Renderer2} from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
+import {debounceTime, distinctUntilChanged, Subject, switchMap} from "rxjs";
+import {ScheduleService} from "../../services/schedule.service";
+import {Schedule} from "../../models/schedule.model";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  searchResults: string[] = [];
+  private searchTerms = new Subject<string>();
 
+  constructor(private scheduleService: ScheduleService) {}
+
+  ngOnInit() {
+    this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => {
+        return this.scheduleService.getSchedules().pipe(
+          map(schedules => this.getUniqueArrivalCities(schedules, term))
+        );
+      })
+    ).subscribe(filteredCities => {
+      this.searchResults = filteredCities;
+    });
+  }
+
+  onSearch(event: Event) {
+    const searchTerm = (event.target as HTMLInputElement).value;
+    this.searchTerms.next(searchTerm);
+  }
+
+  private getUniqueArrivalCities(schedules: Schedule[], searchTerm: string): string[] {
+    return [...new Set(schedules
+      .map(schedule => schedule.route?.arrivalCity)
+      .filter(city => city && city.toLowerCase().includes(searchTerm.toLowerCase()))
+    )];
+  }
 }
