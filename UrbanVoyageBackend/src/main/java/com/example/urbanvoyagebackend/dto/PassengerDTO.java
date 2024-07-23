cd8260f2845fdb572ec611ce2f5036d67eb8fb41
itserror808 <meftahahmedreda02@gmail.com>
2024-07-23 18:02:43 +0100

    refund/change ticket -> i get null on reservation id

---
+++ UrbanVoyageBackend/src/main/java/com/example/urbanvoyagebackend/dto/PassengerDTO.java
@@ -0,0 +1,107 @@
+package com.example.urbanvoyagebackend.dto;
+
+import java.math.BigDecimal;
+
+public class PassengerDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private Long reservationId;
+    private String specialRequests;
+    private String departureCity;
+    private String arrivalCity;
+    private BigDecimal SchedulePrice;
+    private String seatType;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(Long reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public String getSpecialRequests() {
+        return specialRequests;
+    }
+
+    public void setSpecialRequests(String specialRequests) {
+        this.specialRequests = specialRequests;
+    }
+
+    public String getDepartureCity() {
+        return departureCity;
+    }
+
+    public void setDepartureCity(String departureCity) {
+        this.departureCity = departureCity;
+    }
+
+    public String getArrivalCity() {
+        return arrivalCity;
+    }
+
+    public void setArrivalCity(String arrivalCity) {
+        this.arrivalCity = arrivalCity;
+    }
+
+    public BigDecimal getSchedulePrice() {
+        return SchedulePrice;
+    }
+
+    public void setSchedulePrice(BigDecimal schedulePrice) {
+        SchedulePrice = schedulePrice;
+    }
+
+    public String getSeatType() {
+        return seatType;
+    }
+
+    public void setSeatType(String seatType) {
+        this.seatType = seatType;
+    }
+
+    // Getters and setters
+}
