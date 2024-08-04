c63ac16ba928c81172ac105b6130f97b8637efb1
itserror808 <meftahahmedreda02@gmail.com>
2024-08-04 11:42:17 +0100

    zwaw9 n3amas, lmap

--- UrbanVoyageFrontend/src/app/components/morocco-map/morocco-map.component.ts
+++ UrbanVoyageFrontend/src/app/components/morocco-map/morocco-map.component.ts
@@ -9,7 +9,7 @@ import { locations } from '../../data/locations.data';
 })
 export class MoroccoMapComponent implements OnInit, AfterViewInit {
   @Output() citiesSelected = new EventEmitter<{ departure: string, arrival: string, distance: number }>();
-  @Output() closeModal = new EventEmitter<void>(); // Add this line
+  @Output() closeModal = new EventEmitter<void>();
   @Output() searchRoutes = new EventEmitter<void>();
 
   private map!: L.Map;
@@ -38,8 +38,8 @@ export class MoroccoMapComponent implements OnInit, AfterViewInit {
       zoomDelta: 0.5
     });
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; OpenStreetMap contributors'
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
     this.addCustomControls();
@@ -57,7 +57,14 @@ export class MoroccoMapComponent implements OnInit, AfterViewInit {
       },
       onAdd: () => {
         const container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
-        container.innerHTML = '<button class="reset-zoom">Reset View</button>';
+        container.innerHTML = `
+          <button class="reset-zoom bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block mr-1" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M4 2a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V4a2 2 0 00-2-2H4zm3 3a1 1 0 011-1h4a1 1 0 110 2H8a1 1 0 01-1-1z" clip-rule="evenodd" />
+            </svg>
+            Reset View
+          </button>
+        `;
         container.onclick = () => {
           this.map.setView([31.7917, -7.0926], 6);
         };
@@ -72,9 +79,13 @@ export class MoroccoMapComponent implements OnInit, AfterViewInit {
     locations.forEach(location => {
       const cityIcon = L.divIcon({
         className: 'city-marker',
-        html: `<div class="city-label">${location.name}</div>`,
-        iconSize: [100, 20],
-        iconAnchor: [50, 10]
+        html: `
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#14b8a6" width="24" height="24">
+            <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
+          </svg>
+        `,
+        iconSize: [24, 24],
+        iconAnchor: [12, 24]
       });
 
       const marker = L.marker([location.lat, location.lng], { icon: cityIcon })
@@ -144,17 +155,30 @@ export class MoroccoMapComponent implements OnInit, AfterViewInit {
 
       if (departure && arrival) {
         this.clearRoute();
-        this.routeLine = L.polyline([
-          [departure.lat, departure.lng],
-          [arrival.lat, arrival.lng]
-        ], {
+        this.routeLine = L.polyline([], {
           color: '#14b8a6',
           weight: 4,
           opacity: 0.8,
           dashArray: '10, 10'
         }).addTo(this.map);
 
-        this.map.fitBounds(this.routeLine.getBounds(), { padding: [50, 50], duration: 0.3 });
+        const points = [
+          [departure.lat, departure.lng],
+          [arrival.lat, arrival.lng]
+        ];
+
+        let i = 0;
+        const animateRoute = () => {
+          if (i < points.length) {
+            this.routeLine?.addLatLng(points[i] as L.LatLngExpression);
+            i++;
+            requestAnimationFrame(animateRoute);
+          } else {
+            this.map.fitBounds(this.routeLine!.getBounds(), { padding: [50, 50], duration: 0.3 });
+          }
+        };
+
+        animateRoute();
         this.distance = this.calculateDistance(departure, arrival);
       }
     }
@@ -205,10 +229,8 @@ export class MoroccoMapComponent implements OnInit, AfterViewInit {
     }
     this.searchRoutes.emit();
   }
+
   closeMapModal(): void {
     this.closeModal.emit();
   }
-
-
-
 }
